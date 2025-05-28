e947c70992432bcc578efc446f5b9626a7aacf9d
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-05-28 03:24:23 -0300

    feat: added route and sendfile index

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
 
-// Endpoint GET que recebe parâmetro opcional "date"
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 app.get('/api/:date?', (req, res) => {
   const dateParam = req.params.date;
 
   let date;
 
   if (!dateParam) {
-    // Sem parâmetro, data atual
+    // Sem parâmetro: data atual
     date = new Date();
   } else {
-    // Verifica se o parâmetro é um número (timestamp)
+    // Se o parâmetro for um número inteiro (timestamp em milissegundos)
     if (/^\d+$/.test(dateParam)) {
-      // Converte para número inteiro
+      // Criar data a partir do timestamp numérico
       date = new Date(parseInt(dateParam));
     } else {
       // Tenta criar data a partir da string
@@ -21,19 +31,26 @@ app.get('/api/:date?', (req, res) => {
     }
   }
 
-  // Verifica se a data é válida
+  // Verifica se a data é inválida
   if (date.toString() === "Invalid Date") {
     return res.json({ error: "Invalid Date" });
   }
 
-  // Retorna JSON com unix e utc
+
+  console.log("🚀 ~ app.get ~ date:", date)
+
+  // Resposta JSON com unix e utc
   res.json({
     unix: date.getTime(),
     utc: date.toUTCString()
   });
 });
 
-// Define porta e inicia servidor
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
